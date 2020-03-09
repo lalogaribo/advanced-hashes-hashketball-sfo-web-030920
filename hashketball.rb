01c5d1567e20d6dfb2068cd29b67d6efd883fc00
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-03-09 17:20:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,6 +114,7 @@ end
 
 def num_points_scored(player_name)
   game_hash.each do |location, team|
+    print player_name
    return player[:player_name] if team[:players].include?(player_name)
   end
   return nil
